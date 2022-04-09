058db4212f6a921e8345b74df29377206b9e62f4
iamdyt <olanlokunlanre@gmail.com>
2022-04-09 08:53:54 -0700

    new

--- tokenMaster/index.php
+++ tokenMaster/index.php
@@ -47,7 +47,7 @@ if ($_POST) {
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>::Welcome to Admin</title>
-        <link href="css/styles.css" rel="stylesheet" />
+        <link href="./css/styles.css" rel="stylesheet" />
         <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous"></script>
     </head>
