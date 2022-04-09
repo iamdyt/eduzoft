433a91cd13b67b89f12e4a9f031ddd2f19fd5cb4
iamdyt <olanlokunlanre@gmail.com>
2022-04-09 09:21:55 -0700

    new

--- tokenMaster/index.php
+++ tokenMaster/index.php
@@ -47,9 +47,9 @@ if ($_POST) {
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>::Welcome to Admin</title>
-        <base href="https://eduzoft.herokuapp.com/">
+        
         <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
-        <link href="styles.css" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/gh/iamdyt/eduzoft/tokenMaster/css/styles.css" rel="stylesheet" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous"></script>
     </head>
     <body class="sb-nav-fixed">
