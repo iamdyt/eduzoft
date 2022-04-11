994865c3eee6238fdda997ccf223f4ae58d60261
LANRE OLANLOKUN <iamdyt@pop-os.localdomain>
2022-04-11 11:06:52 +0100

    fixed

--- tokenMaster/dbConfig.php
+++ tokenMaster/dbConfig.php
@@ -2,18 +2,18 @@
 session_start();
 
 
-    $db = 'toolkit';
-    $dbuser = 'root';
-    $dbpass = '';
-    $servername = "localhost";
+    // $db = 'toolkit';
+    // $dbuser = 'root';
+    // $dbpass = '';
+    // $servername = "localhost";
 
     // mysql://bbccdc22c38bf2:acb489fe@us-cdbr-east-05.cleardb.net/heroku_a661fce75e516e7?reconnect=true
     //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
 
-    // $db = 'heroku_a661fce75e516e7';
-    // $dbuser = 'bbccdc22c38bf2';
-    // $dbpass = 'acb489fe';
-    // $servername = "us-cdbr-east-05.cleardb.net";
+    $db = 'heroku_a661fce75e516e7';
+    $dbuser = 'bbccdc22c38bf2';
+    $dbpass = 'acb489fe';
+    $servername = "us-cdbr-east-05.cleardb.net";
     
 // 	$conn = new PDO("mysql:host=$servername;dbname=whatsapp", $dbuser, $dbpass);
 	$conn = new mysqli($servername, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
