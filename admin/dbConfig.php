047bc1fca8f36b97476077d280185db02536b639
LANRE OLANLOKUN <iamdyt@pop-os.localdomain>
2022-04-22 13:34:33 +0100

    all-except-loginlimit

--- admin/dbConfig.php
+++ admin/dbConfig.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 
-
     // $db = 'newtoolkit';
     // $dbuser = 'root';
     // $dbpass = '';
@@ -25,4 +24,21 @@ session_start();
 if(!$conn){
     die('Database connection error');
 }
+
+function forceHTTPS(){
+    $httpsURL = 'https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+    if( count( $_POST )>0 )
+      die( 'Page should be accessed with HTTPS, but a POST Submission has been sent here. Adjust the form to point to '.$httpsURL );
+    if( !isset( $_SERVER['HTTPS'] ) || $_SERVER['HTTPS']!=='on' ){
+      if( !headers_sent() ){
+        header( "Status: 301 Moved Permanently" );
+        header( "Location: $httpsURL" );
+        exit();
+      }else{
+        die( '<script type="javascript">document.location.href="'.$httpsURL.'";</script>' );
+      }
+    }
+  }
+
+  forceHTTPS();
 ?>
